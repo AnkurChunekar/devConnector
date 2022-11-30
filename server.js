0ce8337cfdff165ff14240c522583b5d6b355a8b
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-11-30 22:59:00 +0530

    feat: initialize db connection

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 require("dotenv").config();
+const { initializeDbConnection } = require("./db/db.connect");
 
 const app = express();
 const PORT = process.env.PORT;
+
+initializeDbConnection();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
