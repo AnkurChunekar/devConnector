3bed00d2c86e096c5e58434f8d7085affad439e2
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-12-02 22:41:49 +0530

    feat: validate req.body fields

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 /* 
 @route POST api/users
@@ -7,9 +8,22 @@ const router = express.Router();
 @access Public
 */
 
-router.route("/").post((req, res) => {
-  const body = req.body;
-  res.json(body);
+const validators = [
+  check("email", "email is invalid").isEmail(),
+  check("name", "please provide a valid name").not().isEmpty(),
+  check(
+    "password",
+    "password length should be minimum 6 characters long"
+  ).isLength({ min: 6 }),
+];
+
+router.route("/").post(validators, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  } else {
+    res.json({ reqBody: req.body });
+  }
 });
 
 module.exports = router;
