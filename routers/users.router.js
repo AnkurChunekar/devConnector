e48932e28c1a12e244ff84941d89fe868b3cb947
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-12-02 22:10:12 +0530

    feat: add post route for users and consume body

--- routers/users.router.js
+++ routers/users.router.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 /* 
-@route GET api/users
-@desc Test route
+@route POST api/users
+@desc Add a new user
 @access Public
 */
 
-router.route("/").get((req, res) => {
-  res.send("Users here");
+router.route("/").post((req, res) => {
+  const body = req.body;
+  res.json(body);
 });
 
 module.exports = router;
