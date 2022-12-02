d5170920eec0f966560c915a3df7ac41ea2e8a1b
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-12-02 21:56:50 +0530

    feat: consume and test all routers

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
+// external
 const express = require("express");
 require("dotenv").config();
+
+// internal
 const { initializeDbConnection } = require("./db/db.connect");
+const authRouter = require("./routers/auth.router");
+const postsRouter = require("./routers/posts.router");
+const usersRouter = require("./routers/users.router");
+const profileRouter = require("./routers/profile.router");
 
 const app = express();
 const PORT = process.env.PORT;
 
+// connect to db
 initializeDbConnection();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/profile", profileRouter);
 
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
