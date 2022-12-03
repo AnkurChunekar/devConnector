a61734cacb37dccfaf498c5196dcf8f5ba04ce06
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-12-03 12:41:17 +0530

    feat: update users post to add a new user

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 const { validationErrMsgs } = require("../constants/stringConstants");
+const UserModel = require("../models/User.model");
+const { getErrorsObj } = require("../utillities/utils");
 
 /* 
 @route POST api/users
@@ -16,13 +20,37 @@ const validators = [
   check("password", validationErrMsgs.password).isLength({ min: 6 }),
 ];
 
-router.route("/").post(validators, (req, res) => {
+router.route("/").post(validators, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
-  } else {
-    res.json({ reqBody: req.body });
   }
+
+  const { email, password, name } = req.body;
+
+  let user = await UserModel.findOne({ email });
+
+  if (user) return res.status(400).json(getErrorsObj("user already exists"));
+
+  const avatar = gravatar.url(email, {
+    s: "200",
+    r: "pg",
+    d: "mm",
+  });
+
+  user = new UserModel({
+    name,
+    email,
+    password,
+    avatar,
+  });
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hashSync(password, salt);
+
+  await user.save();
+
+  res.json({ message: "user registered successfully" });
 });
 
 module.exports = router;
