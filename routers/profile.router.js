a4c7087c2314e112f6a0dfc249e4c88c4e0abd57
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-12-11 22:04:51 +0530

    feat: add route to get user profile data

--- routers/profile.router.js
+++ routers/profile.router.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const authVerify = require("../middlewares/authVerify.middleware");
 const router = express.Router();
+const Profile = require("../models/Profile.model");
+const { getErrorsObj } = require("../utillities/utils");
 
 /* 
-@route GET api/profile
-@desc Test route
-@access Public
+@route GET api/profile/me
+@desc Get current users profile data.
+@access Private
 */
 
-router.route("/").get((req, res) => {
+router.route("/me").get(authVerify, async (req, res) => {
+  const profile = await Profile.findOne({ user: req.user.userId }).populate(
+    "user",
+    ["name", "avatar"]
+  );
+
+  if (!profile) res.status(400).json(getErrorsObj("Profile doesn't exists"));
+  else res.json({ profile });
+
   res.send("Profile here");
 });
 
