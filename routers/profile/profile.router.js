e9ac4f194cca5ef839cb38a363231de807cc3134
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-12-18 15:01:16 +0530

    refactor: profile routes

--- routers/profile/profile.router.js
+++ routers/profile/profile.router.js
@@ -1,34 +1,15 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
 const router = express.Router();
 
 const authVerify = require("../../middlewares/authVerify.middleware");
 const Profile = require("../../models/Profile.model");
-const User = require("../../models/User.model");
-const { getErrorsObj, getSanitizedObj } = require("../../utillities/utils");
-const experienceRouter = require("./experience.router");
-const educationRouter = require("./education.router");
 
-const socialsArr = ["linkedin", "twitter", "youtube", "facebook", "instagram"];
+const { getErrorsObj } = require("../../utillities/utils");
+const myProfileRouter = require("./myProfile.router");
 
-const profileKeysArr = [
-  "company",
-  "website",
-  "location",
-  "bio",
-  "status",
-  "githubUsername",
-  "skills",
-  "social",
-];
-
-/* 
-
-@route Get api/profiles
-@desc Get all user profiles
-@access Public
-
-*/
+// @route Get api/profiles
+// @desc Get all user profiles
+// @access Public
 
 router.route("/").get(async (req, res) => {
   try {
@@ -40,13 +21,9 @@ router.route("/").get(async (req, res) => {
   }
 });
 
-/* 
-
-@route Get api/profiles/user/:userId
-@desc Get profile for given user id
-@access Public
-
-*/
+// @route Get api/profiles/user/:userId
+// @desc Get profile for given user id
+// @access Public
 
 router.route("/user/:userId").get(async (req, res) => {
   try {
@@ -68,98 +45,6 @@ router.route("/user/:userId").get(async (req, res) => {
   }
 });
 
-const validators = [
-  check("status", "status is required").not().isEmpty(),
-  check("skills", "skills are required").not().isEmpty(),
-];
-
-router
-  .route("/me")
-
-  // @route GET api/profiles/me
-  // @desc Get current users profile data.
-  // @access Private
-
-  .get(authVerify, async (req, res) => {
-    try {
-      const profile = await Profile.findOne({ user: req.user.userId }).populate(
-        "user",
-        ["name", "avatar"]
-      );
-      if (!profile)
-        res.status(400).json(getErrorsObj("Profile doesn't exists"));
-      else res.json({ profile });
-    } catch (error) {
-      res.status(500).json(getErrorsObj(error.message));
-    }
-  })
-
-  // @route POST api/profiles/me
-  // @desc Create or update user profile
-  // @access Private
-
-  .post(authVerify, validators, async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    const { userId } = req.user;
-    const profileObj = getSanitizedObj(profileKeysArr, req.body);
-
-    if (profileObj.skills)
-      profileObj.skills = profileObj.skills
-        .split(",")
-        .map((item) => item.trim());
-
-    if (profileObj.socials) {
-      profileObj.socials = getSanitizedObj(socialsArr, profileObj.socials);
-    }
-
-    let profile = await Profile.findOne({ user: userId });
-
-    try {
-      if (profile) {
-        //  update
-        // { user: userId } filter : finds the profile where user field has the given id
-        // { $set: profileObj }
-        // { new: true } returns the newly generated document if not passed by default the older one is returned
-
-        profile = await Profile.findOneAndUpdate(
-          { user: userId },
-          { $set: profileObj },
-          { new: true }
-        );
-      } else {
-        // add new
-        profileObj.user = userId;
-        profile = await new Profile(profileObj);
-        profile.save();
-      }
-
-      res.json(profile);
-    } catch (error) {
-      res.status(500).json(getErrorsObj(error.message));
-    }
-  })
-
-  // @route DELETE api/profiles/me
-  // @desc deletes both current user data and profile
-  // @access Private
-
-  .delete(authVerify, async (req, res) => {
-    try {
-      const { userId } = req.user;
-      await Profile.findOneAndRemove({ user: userId });
-      await User.findOneAndRemove({ _id: userId });
-
-      res.json({ message: "user deleted successfully" });
-    } catch (error) {
-      res.status(500).json(getErrorsObj(error.message));
-    }
-  });
-
-router.use("/me/experience", authVerify, experienceRouter);
-router.use("/me/education", authVerify, educationRouter);
+router.use("/me", authVerify, myProfileRouter);
 
 module.exports = router;
