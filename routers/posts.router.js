ffa996329ff2cfff01c4cb0de01d5e54144a671d
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-12-19 22:20:15 +0530

    feat: add route to get all posts

--- routers/posts.router.js
+++ routers/posts.router.js
@@ -11,35 +11,49 @@ const VALIDATORS = {
   post: [check("text", "text is required").not().isEmpty()],
 };
 
-/* 
-@route POSTS api/posts
-@desc Test route
-@access Public
-*/
+// @route POST api/posts
+// @desc add a new post
+// @access Private
 
-router.route("/").post(VALIDATORS.post, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+router
+  .route("/")
+  .post(VALIDATORS.post, async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const { userId } = req.user;
+
+      const user = await User.findById(userId).select("-password");
+      const post = new Post({
+        text: req.body.text,
+        user: userId,
+        name: user.name,
+        avatar: user.avatar,
+      });
+
+      const savedPost = await post.save();
+      res.json(savedPost);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json(getErrorsObj(error.message));
     }
+  })
 
-    const { userId } = req.user;
+  // @route GET api/posts
+  // @desc Get all the posts sorted in latest first format.
+  // @access Private
 
-    const user = await User.findById(userId).select("-password");
-    const post = new Post({
-      text: req.body.text,
-      user: userId,
-      name: user.name,
-      avatar: user.avatar,
-    });
-
-    const savedPost = await post.save();
-    res.json(savedPost);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json(getErrorsObj(error.message));
-  }
-});
+  .get(async (req, res) => {
+    try {
+      const posts = await Post.find().sort({ date: -1 });
+      res.json({ posts });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json(getErrorsObj(error.message));
+    }
+  });
 
 module.exports = router;
