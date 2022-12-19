8691b71167a61d5930c2c4a79e3636f70031d06e
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-12-19 22:39:40 +0530

    feat: add route to get single post and delet single post

--- routers/posts.router.js
+++ routers/posts.router.js
@@ -3,7 +3,7 @@ const { check, validationResult } = require("express-validator");
 
 const Post = require("../models/Post.model");
 const User = require("../models/User.model");
-const { getErrorsObj, getSanitizedObj } = require("../utillities/utils");
+const { getErrorsObj } = require("../utillities/utils");
 
 const router = express.Router();
 
@@ -52,6 +52,54 @@ router
       res.json({ posts });
     } catch (error) {
       console.log(error.message);
+      res.status(500).json(getErrorsObj(error.message));
+    }
+  });
+
+// @route GET api/posts/:id
+// @desc Get single post with the given id
+// @access Private
+
+router
+  .route("/:id")
+  .get(async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+
+      if (!post) return res.status(404).json(getErrorsObj("Post not found"));
+
+      res.json({ post });
+    } catch (error) {
+      console.log(error.message);
+      if (error.kind === "ObjectId")
+        return res.status(404).json(getErrorsObj("Post not found"));
+
+      res.status(500).json(getErrorsObj(error.message));
+    }
+  })
+
+  // @route DELETE api/posts/:id
+  // @desc Delete single post with the given id
+  // @access Private
+
+  .delete(async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+
+      if (!post) return res.status(404).json(getErrorsObj("Post not found"));
+
+      // authorize user
+      if (post.user.toString() !== req.user.userId)
+        return res.status(401).json(getErrorsObj("User not authorized"));
+
+      await post.remove();
+
+      res.json({ message: "post deleted successfully" });
+    } catch (error) {
+      console.log(error.message);
+      if (error.kind === "ObjectId")
+        return res.status(404).json(getErrorsObj("Post not found"));
+
       res.status(500).json(getErrorsObj(error.message));
     }
   });
